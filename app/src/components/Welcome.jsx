23b7835f56d059df8b128db34217a3987fb99a39
Brad Nguyen <thienan.nguyen@lilly.com>
2024-07-10 19:06:06 -0400

    fix styling for welcome page

--- app/src/components/Welcome.jsx
+++ app/src/components/Welcome.jsx
@@ -10,21 +10,24 @@ function Welcome() {
   }, []);
 
   return (
-    <main className={`flex flex-col justify-center min-h-screen`}>
+    <main className={`flex flex-col justify-center min-h-screen text-white`}>
+      <div 
+       className="min-w-full min-h-screen fixed -z-10 opacity-30 bg-black"
+      />
       <img
         alt="lilly scientist"
         src="https://cdn.thepharmaletter.com/files/2022/12/819984c0-74c3-11ed-8ccd-df2a7a949035-eli_lilly_science_large.jpg"
-        className="opacity-50 fixed -z-10 w-screen h-screen blur-md"
+        className="opacity-50 fixed -z-20 w-screen h-screen blur-md"
       />
       <div className="py-4 z-10">
-        <h1 className="text-5xl text-center">LillyOnboard</h1>
-        <h2 className="pt-5 w-8/12 mx-auto text-3xl text-center">
+        <h1 className="text-5xl text-center drop-shadow-xl">LillyOnboard</h1>
+        <h2 className="pt-5 w-8/12 mx-auto text-3xl text-center drop-shadow-xl">
           One stop shop for new employees
         </h2>
       </div>
 
       <div className="pt-5 flex justify-center">
-        <Link to={`${role}`} className="border-[1px] p-2.5 rounded-2xl text-xl">
+        <Link to={`${role}`} className="border-[1px] p-2.5 rounded-2xl text-xl drop-shadow-xl">
           Getting Started
         </Link>
       </div>
